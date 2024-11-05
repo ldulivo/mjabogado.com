68701012fa343ec7ef94dd58020e588e9c29c770
ldulivo <ldulivo@gmail.com>
2024-11-06 00:09:01 +0100

    correcciones bordes, navbar. Add font Jost

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,12 @@ menuToggle.addEventListener('change', () => {
     document.body.style.paddingRight = '';
   }
 });
+
+window.addEventListener('scroll', () => {
+  const nav = document.querySelector('nav');
+  if (window.scrollY > 50) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
