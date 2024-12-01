344accd396b63f41c6d0e5e909d357f33eadcdd6
ldulivo <ldulivo@gmail.com>
2024-12-02 00:47:53 +0100

    creando botón whatsApp

---
+++ assets/js/whatsapp.js
@@ -0,0 +1,42 @@
+const whatsapp = document.getElementById('whatsapp');
+const whatsappButton = document.getElementById('whatsappButton');
+const whatsappDialog = document.getElementById('whatsappDialog');
+const whatsappNumber = document.getElementById('whatsappNumber');
+
+function isMobileDevice() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+function openDialog() {
+  // e.stopPropagation();
+  whatsapp.querySelector('.whatsapp--content').style.display = 'none'
+  whatsappDialog.showModal();
+}
+
+function closeDialog(e) {
+  if (e.target.closest('#whatsappButtonClose')) {
+  whatsapp.querySelector('.whatsapp--content').style.display = 'block'
+    whatsappDialog.close();
+  }
+}
+
+
+function loadWhatsapp() {
+  whatsappDialog.addEventListener('click', closeDialog);
+  whatsappNumber.innerText = whatsappButton.dataset.number;
+
+  if (!isMobileDevice()) {
+    whatsapp.classList.add('whatsapp--dialog');
+    whatsappButton.classList.add('whatsapp--desktop');
+    whatsappButton.addEventListener('click', openDialog);
+  }
+
+  if (isMobileDevice()) {
+    const href = 'https://wa.me/' + whatsappNumber.innerText + '?text=Hola, ¿cómo te puedo ayudar?';
+    whatsappButton.href = href;
+    whatsappButton.target = '_blank';
+    whatsappButton.rel = 'noopener noreferrer';
+  }
+}
+
+loadWhatsapp();
